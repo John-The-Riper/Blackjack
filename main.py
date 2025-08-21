92ee462332048117b5440163f1a33b12e5e4bc1d
Owen <owen106225@mcpsva.org>
2025-08-21 17:15:23 -0400

    v2 running screen

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-# Example file showing a basic pygame "game loop"
 import pygame
+import random
+import sys
 
-# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+# Colors for the game screen
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 100, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
 
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
+# Game variables and functions
+def screen_settings():
+    screen = pygame.display.set_mode((475, 475))
+    screen.fill((GREEN))
     pygame.display.flip()
+    return screen
 
-    clock.tick(60)  # limits FPS to 60
 
-pygame.quit()
+def deck_of_cards():
+    deck = []
+
+
+# Start game has the while loop that runs the game
+def start_game():
+    running = True
+    screen_settings()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+    pygame.quit()
+
+
+start_game()
+sys.exit()
