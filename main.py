378a18ff8f74a2d23a3d29c8d731fc2fc0ecb28f
Owen <owen106225@mcpsva.org>
2025-08-21 18:36:52 -0400

    v2 running screen

--- main.py
+++ main.py
@@ -12,17 +12,13 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GRAY = (128, 128, 128)
 
-
 # Game variables and functions
 def screen_settings():
     screen = pygame.display.set_mode((475, 475))
+    pygame.display.set_caption("Blackjack")
     screen.fill((GREEN))
     pygame.display.flip()
     return screen
-
-
-def deck_of_cards():
-    deck = []
 
 
 # Start game has the while loop that runs the game
@@ -36,9 +32,19 @@ def start_game():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-
+            elif event.type == pygame.K_1:
+                player_hit()
     pygame.quit()
 
 
 start_game()
+player_hand = 0
+
+def player_hit():
+    global player_hand
+    player_hand += 1
+    new_value = player_hand
+    return new_value
+
+print(player_hand)
 sys.exit()
