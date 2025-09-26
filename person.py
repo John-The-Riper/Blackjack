9b529f8f925bcff2bd28b0e92f316b47a9a15341
John-The-Riper <owen106225@mcpsva.org>
2025-09-26 16:35:56 -0400

    finished

--- person.py
+++ person.py
@@ -1,17 +1,27 @@
-import random
 class Person:
     def __init__(self, money):
         self.money = money
         self.cards = []
-        self.card_value = 0
 
     def get_hand(self):
         return self.cards
 
     def add_card(self, card):
-       #print(card.get_name())
         self.cards.append(card)
-        self.card_value += card.get_value()
 
     def get_value(self):
-        return self.card_value
+        total = 0
+        aces = 0
+
+        for card in self.cards:
+            value = card.get_value()
+            total += value
+            if value == 11:  # count Ace
+                aces += 1
+
+        # If bust, downgrade Ace(s) from 11 to 1 until safe or no more Aces
+        while total > 21 and aces > 0:
+            total -= 10  # effectively makes an Ace count as 1 instead of 11
+            aces -= 1
+
+        return total
