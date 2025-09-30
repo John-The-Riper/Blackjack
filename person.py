22fba768e357123fe6574239dd188b70d3044c91
John-The-Riper <owen106225@mcpsva.org>
2025-09-30 09:55:15 -0400

    Update person.py

--- person.py
+++ person.py
@@ -19,9 +19,8 @@ class Person:
             if value == 11:  # count Ace
                 aces += 1
 
-        # If bust, downgrade Ace(s) from 11 to 1 until safe or no more Aces
         while total > 21 and aces > 0:
-            total -= 10  # effectively makes an Ace count as 1 instead of 11
+            total -= 10 
             aces -= 1
 
         return total
