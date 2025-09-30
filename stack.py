3f3df2954f66dfd7d94329dee6ea452dfff45930
Owen106225 <owen106225@mcpsva.org>
2025-09-30 09:52:41 -0400

    ??

---
+++ stack.py
@@ -0,0 +1,11 @@
+"""
+Filename: Stack.py
+Author: <McDougal, Owen>
+Created: <09/30/2025>
+Instructor: Holtslander
+"""
+class stack:
+    def __init__(self):
+
+    class node:
+        def __init__(self):
